54c2a460ab6f9ff098d813e1916d7b9a244e7b93
Carlos Vieira <cvieira.eduardo@gmail.com>
2021-03-17 15:00:09 -0300

    Improving fingerprint

--- larasploit.py
+++ larasploit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import requests, sys
+import urllib3
+urllib3.disable_warnings()
+from bs4 import BeautifulSoup
 
 class colors:
     HEADER = '\033[95m'
@@ -16,7 +19,6 @@ class colors:
 def banner():
 	print(colors.OKGREEN)
 	print("""
-.-.. .- .-. .- ... .--. .-.. --- .. - 
 
 ______                                     ______     __________ 
 ___  / ______ _____________ __________________  /________(_)_  /_
@@ -27,4 +29,98 @@ _  /___/ /_/ /_  /   / /_/ /_(__  )__  /_/ /  / / /_/ /  / / /_
 	- Laravel Automated Vulnerability Scanner
 	""")
 	print(colors.HEADER)
+    # 7468 6 52071 7569 6 574 6 57 2 2 07 9 6f7 5 2062 6 5636f 6 d652 c 206 d 6f 7 2 6 52 0 796 f 7520 6 17265 2 0616 2 6c6 5 20 7 4 6f 2 068 6561 7 20a
+def fingerprint(host):
+    
+    fingerprint_data = []
 
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+    proxy = {}
+
+    response = requests.get(host, headers=headers, proxies=proxy, verify=False, allow_redirects=True)
+    print(f'{colors.HEADER}')
+    print(f"{colors.OKGREEN} [~] Application Fingerprint {colors.HEADER}\r\n")
+
+    print(f'{colors.OKGREEN} [HTTP STATUS]: {colors.HEADER} {response.status_code}')
+
+    if('Location' in response.headers):
+        print(f'{colors.OKGREEN} [HTTP Redirect]: {colors.HEADER} {response.headers["Location"]}')
+
+
+    if('server' in response.headers and response.headers['Server']):
+        fingerprint_data.append({'server': response.headers['server']})
+
+        print(f'{colors.OKGREEN} [Server]: {colors.HEADER} {response.headers["Server"]}')
+
+    if('X-Powered-By' in response.headers and 'PHP' in response.headers['X-Powered-By']):
+        fingerprint_data.append({'php_version': response.headers['X-Powered-By']})
+
+        print(f'{colors.OKGREEN} [PHP Version]: {colors.HEADER} {response.headers["X-Powered-By"]}')
+
+    for cookie in dict(response.cookies):
+            if('XSRF-TOKEN' in cookie or '_session' in cookie):
+                fingerprint_data.append({cookie: response.cookies[cookie]})
+                print(f'{colors.OKGREEN} [Common Laravel Cookie]: {colors.HEADER} {cookie}: {response.cookies[cookie][:20]}...')
+    
+    if('_ignition\/' in response.text):
+            print(f'{colors.WARNING} [Info]: {colors.HEADER} Laravel 8 detected (with ignition)!')
+    
+    if('Laravel v8' in response.text):
+            print(f'{colors.WARNING} [Info]: {colors.HEADER} Laravel 8 detected!')
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    laravel_version = ""
+    for searchWrapper in soup.find_all('div', {'class':'ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0'}):
+        laravel_version = searchWrapper.text.strip()
+
+    if(laravel_version):
+        print(f'{colors.WARNING} [Info]: {colors.HEADER} Default laravel instalation detected!')
+        print(f'{colors.WARNING} [Version]: {colors.HEADER} {laravel_version}')
+
+    laravel_default = ""
+    for searchWrapper in soup.find_all('div', {'class':'title m-b-md'}):
+        text = searchWrapper.text.strip()
+        if(text == "Laravel"):
+            laravel_default = True
+
+    for searchWrapper in soup.find_all('div', {'class':'links'}):
+        text = searchWrapper.find('a').text.strip()
+        if(text == "Laravel" or text == "Docs"):
+            laravel_default = True
+
+    if(laravel_default):
+        print(f'{colors.WARNING} [Info]: {colors.HEADER} Default laravel instalation detected!')
+        print(f'{colors.WARNING} [Version]: {colors.HEADER} Laravel < 7')
+
+   
+    env_testing = requests.get(host + "/.env", headers=headers, proxies=proxy, verify=False)
+    if(env_testing.status_code == 200):
+        if('APP_ENV' in env_testing.text):
+            print(f"{colors.FAIL} [!] Vulnerability detected: .env exposed\n")
+
+            for env_line in env_testing.text.split('\n'):
+                if('APP_ENV' in env_line):
+                    if(env_line != 'APP_ENV=production'):
+                        print(f'{colors.WARNING} [Info]: {colors.HEADER} Application running without production mode!')
+
+                if("APP_KEY" in env_line):
+                    print(f'{colors.WARNING} [Info]: {colors.HEADER} APP_KEY leaked: {env_line}')
+
+                if("APP_DEBUG" in env_line):
+                    if(env_line == "APP_DEBUG=true"):
+                        print(f'{colors.WARNING} [Info]: {colors.HEADER} Application running with Debug Mode')
+                    else:
+                        print(f'{colors.WARNING} [Info]: {colors.HEADER} Application running without Debug Mode')
+
+    return fingerprint_data
+def main():
+    banner()
+    if(len(sys.argv) > 1):
+        fp = fingerprint(sys.argv[1])
+        #print(fp)
+    else:
+        print(f"{colors.WARNING}[!] USE: {sys.argv[0]} https://target.com\r\n")
+
+
+if __name__ == "__main__":
+    main()
