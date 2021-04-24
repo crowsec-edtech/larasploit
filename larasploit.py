d0040c5dcdd5f7643dc7948783dd44c547a2d1a5
oppsec <daniel.sdm.github@gmail.com>
2021-04-23 21:12:47 -0300

    fix: removed colors classes

--- larasploit.py
+++ larasploit.py
@@ -16,17 +16,6 @@ from utils.agent import get_user_agent
 from rich import print
 from bs4 import BeautifulSoup
 
-class colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 host = ""
 app_key = ""
 
